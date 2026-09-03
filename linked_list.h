b94b4403b40a0067abece711eb51122b0f266e4e
Kai <kb877325@ohio.edu>
2026-09-03 10:26:37 -0400

    WIP

--- linked_list.h
+++ linked_list.h
@@ -6,15 +6,54 @@ template <typename T>
 class LinkedList {
 
 public:
-  LinkedList() : head(NULL) {}
+  LinkedList() : head(NULL) {} 
 
-  // You need to implement the following functions. 
-  // 
-  // ~LinkedList();
-  // void push_front(const T data);
-  // void pop_front();
-  // void reverse();
-  // void print() const;
+  ~LinkedList(){
+  ListNode* current = head;
+    while (current != NULL) {
+    ListNode* nextNode = current->next;
+    delete current;
+    current = nextNode;
+    }
+  }
+
+  void push_front(const T data){
+    ListNode* newNode = new ListNode(data);
+    newNode->next = head;
+    head = newNode;
+  }
+
+  void pop_front(){
+    if (head != NULL) {
+      ListNode* temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+  
+  void reverse(){
+    ListNode* prev = NULL;
+    ListNode* current = head;
+    ListNode* next = NULL;
+
+    while (current != NULL) {
+      next = current->next; // Store the next node
+      current->next = prev; // Reverse the link
+      prev = current;       // Move prev to current
+      current = next;       // Move to the next node
+    }
+    head = prev; // Update head to the new front of the list
+  }
+
+  void print() const{
+    
+    ListNode* current = head;
+    while (current != NULL) {
+      cout << current->data << " ";
+      current = current->next;
+    }
+    cout << endl;
+  }
 
 private:
   
@@ -27,5 +66,3 @@ private:
 
   ListNode* head;
 };
-
-
--- main.cpp
+++ main.cpp
@@ -4,4 +4,32 @@ using namespace std;
 
 int main(int argc, char** argv) {
   LinkedList<int> list;
+  string userInput = "";
+
+  while(userInput != "q"){
+    cout << " ? ";
+    getline(cin, userInput);
+
+    //cout << "You entered: \"" << userInput << "\"" << endl;
+    if(userInput[0] == 'a'){
+      list.push_front(stoi(userInput.substr(2)));
+    }
+    else if(userInput == "r"){
+      list.reverse();
+    }
+    else if(userInput == "p"){
+      list.print();
+    }
+    else if(userInput == "d"){
+      list.pop_front();
+    }
+    else if(userInput == "q"){
+      break;
+    }
+    else{
+      cout << "Invalid input" << endl;
+    }
+  }
+
+  return 0;
 }
