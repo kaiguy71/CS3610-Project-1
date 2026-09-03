1cddc36aa729b3708b99ff95456bacb368648c5b
Kai <kb877325@ohio.edu>
2026-09-03 10:36:59 -0400

    Cleanup

--- linked_list.h
+++ linked_list.h
@@ -8,6 +8,10 @@ class LinkedList {
 public:
   LinkedList() : head(NULL) {} 
 
+  /**
+   * @brief Destroy the Linked List object
+   *  Deletes each node one by one until the list is empty, freeing the memory allocated for each node.
+   */
   ~LinkedList(){
   ListNode* current = head;
     while (current != NULL) {
@@ -17,12 +21,21 @@ public:
     }
   }
 
+  /**
+   * @brief Adds a new node with the specified data to the front of the list.
+   * 
+   * @param data The data to be added to the list.
+   */
   void push_front(const T data){
     ListNode* newNode = new ListNode(data);
     newNode->next = head;
     head = newNode;
   }
 
+  /**
+   * @brief Removes the node at the front of the list, if it exists.
+   * 
+   */
   void pop_front(){
     if (head != NULL) {
       ListNode* temp = head;
@@ -31,11 +44,20 @@ public:
     }
   }
   
+  /**
+   * @brief Reverses the order of the nodes in the linked list without using any additional data structures.
+   */
   void reverse(){
     ListNode* prev = NULL;
     ListNode* current = head;
     ListNode* next = NULL;
 
+    /**
+     * If the list is empty or has only one node, no action is needed. The loop will not execute in these cases, and the head will remain unchanged.
+     * 
+     * In the standard case, the loop iterates through the list, reversing the direction of the links between nodes. 
+     * After the loop, the head pointer is updated to point to the new front of the list (the last node processed).
+     */
     while (current != NULL) {
       next = current->next; // Store the next node
       current->next = prev; // Reverse the link
@@ -45,9 +67,17 @@ public:
     head = prev; // Update head to the new front of the list
   }
 
+  /**
+   * @brief Print each node's data in the list, separated by a space. If the list is empty, prints "Empty\n".
+   */
   void print() const{
     
     ListNode* current = head;
+    if (current == NULL) {
+      cout << "Empty" << endl;
+      return;
+    }
+
     while (current != NULL) {
       cout << current->data << " ";
       current = current->next;
--- main.cpp
+++ main.cpp
@@ -6,26 +6,41 @@ int main(int argc, char** argv) {
   LinkedList<int> list;
   string userInput = "";
 
+  // Get user input until they enter "q" to quit
   while(userInput != "q"){
     cout << " ? ";
     getline(cin, userInput);
 
     //cout << "You entered: \"" << userInput << "\"" << endl;
+
+    // Appending a new node
     if(userInput[0] == 'a'){
+      // cout << "Adding \"" << userInput.substr(2) << "\" to the front of the list." << endl;
+
       list.push_front(stoi(userInput.substr(2)));
     }
+
+    // Reversing the list
     else if(userInput == "r"){
       list.reverse();
     }
+
+    // Printing the list
     else if(userInput == "p"){
       list.print();
     }
+
+    // Deleting the front node
     else if(userInput == "d"){
       list.pop_front();
     }
+
+    // Quitting the program
     else if(userInput == "q"){
       break;
     }
+
+    // Invalid case
     else{
       cout << "Invalid input" << endl;
     }
